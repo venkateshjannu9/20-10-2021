d39809773e28a591a492722e5cb8222537870bd4
venkateshjannu9 <91652197+venkateshjannu9@users.noreply.github.com>
2021-10-20 18:41:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+let js="amazing";
+if(js ==="amazing") alert("javascript is fun!");
+
+40 + 8 + 23 - 10;
+ console.log(40 + 8 + 23 - 10);
+
+ console.log("venkatesh");
+ console.log(23);
+
+ let firstname="jannu";
+ console.log(firstname);
+
+ let myFirstJob="trainer";
+ let myCurrentJob="devoloper";
+
+ let myJOb1="coder";
+ let myJob2="tester";
+
+ console.log(myFirstJob);
+ console.log(myCurrentJob);
+ console.log(myJOb1);
+ console.log(myJob2);
+
+ let javaScriptIsFun=true;
+ console.log(typeof javaScriptIsFun);
+ console.log(typeof true);
+ console.log(typeof 'myFirstJob');
+ console.log(typeof 23);
+
+ javaScriptIsFun='yes!';
+ console.log(typeof javaScriptIsFun);
+
+ let year;
+ console.log(year);
+ console.log(typeof year);
+
+ year=1995;
+ console.log(typeof year);
+
+ let age=23;
+ age=24;
+ console.log(age);
+
+ const birthYear=1997;
+//  birthYear=1998;
+ console.log(birthYear);
+
+ var job="programmer";
+  job="tester";
+  console.log(job);
+// math operators
+  const now = 2021;
+  const ageVenky= now - 1998;
+  const ageRavi= now- 1996;
+  console.log(ageVenky,ageRavi);
+   
+  console.log(ageRavi*2, ageVenky/10**3);
+
+  const firstName= "venkatesh";
+  const lastname= "jannu";
+  console.log(firstName + lastname);
+// assignment operator
+  let x = 10+ 5;
+  x=x*3;
+  x=x+6;
+  console.log(x);
+// comparisionoperators 
+ console.log(ageRavi>ageVenky); // >,>=,<=
+ console.log(ageRavi>=26);
+ const isFullAge = ageRavi>=18;
+ console.log(now-1996>now-2011);
+
+ let a, b;
+ a=b=25-10-5;
+ console.log(a,b);
+
+ const avgAge= (ageRavi+ageVenky)/2;
+ console.log(ageRavi, ageVenky,avgAge);
+//  challemge 1
+// const massMark = 78;
+// const heightMark= 1.69;
+// const massJhon="92";
+// const heightJhon="1.95";
+
+const massMark = 95;
+const heightMark= 1.88;
+const massJhon="85";
+const heightJhon="1.76";
+const BMIMArk=massMark/heightMark**2;
+const BMIJhon=massJhon/(heightJhon*heightJhon);
+const markHeightBMI= BMIMArk>BMIJhon;
+console.log(BMIMArk,BMIJhon,markHeightBMI);
+
+const Firstname="venky";
+const Job="trainer";
+const Birthyear=1998;
+const YEar="2021";
+const venky="I am "+ Firstname+","+(YEar-Birthyear)+" years old" +" " + Job +"!";
+console.log(venky);
+
+console.log("just a regular string");
+console.log("string\nmultilple\nlines");
+
+const AGE=16;
+if (AGE>=18)
+{
+console.log("venkatesh can start driving license");
+}
+else{
+    const YearLeft=18-AGE;
+    console.log("venky is too young. wait another ${YearLeft} years:");
+}
+const Byear=2000;
+let century;
+if(Byear<=2000)
+{
+    century=20;
+}
+else{
+    century=21;
+}
+console.log(century);
